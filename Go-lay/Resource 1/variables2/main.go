37c5e20ad06feb6673054b339a58171d86f56911
Devaansh Chauhan <devaansh.chauhan@mogiio.com>
2024-12-13 01:00:10 +0530

    initial commit--Started Go-lay

---
+++ Go-lay/Resource 1/variables2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string
+	fmt.Println("Variables !!")
+	fmt.Println("Username is: ", username)
+	fmt.Printf("Type of this variable is : %T \n ", username)
+
+	fmt.Println("================")
+	var isBoolean bool
+	fmt.Println("isBoolean is: ", isBoolean)
+	fmt.Printf("Type of this varible is: %T \n", isBoolean)
+
+	fmt.Println("==========")
+	var intVariable int
+	fmt.Println("intVariable is: ", intVariable)
+	fmt.Printf("Type of intVaribale is : %T  \n", intVariable)
+
+}
