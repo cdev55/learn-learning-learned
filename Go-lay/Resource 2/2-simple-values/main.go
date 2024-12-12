441bf794390ad118d3e5c115aff7a7a7f204aaa6
Devaansh Chauhan <devaansh.chauhan@mogiio.com>
2024-12-13 01:09:22 +0530

    Added simple values

---
+++ Go-lay/Resource 2/2-simple-values/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Int
+	fmt.Println(1)
+	fmt.Println(19 + 6)
+	//String
+	fmt.Println("This is a string")
+	//Boolean
+	fmt.Println(true)
+	fmt.Println(false)
+
+	//Float
+	fmt.Println(3.14)
+	fmt.Println(18.67 / 3.75)
+}
