7ad1bb0be86eb48f67f234b8b2d37afdc009b758
Devaansh chauhan <devaansh.chauhan@mogiio.com>
2024-12-20 15:06:50 +0530

    added boilerplate code to blog mini-micro

--- Micro/mini-Micro-App/blog/post/index.js
+++ Micro/mini-Micro-App/blog/post/index.js
@@ -1,10 +1,22 @@
 import express from "express";
+import crypto from "crypto";
+import bodyParser from "body-parser";
 const port = 4000;
 const app = express();
-const posts = {};
 
-app.get("/", (req,res) => {
-  res.send(posts);
+const posts = {};
+app.use(bodyParser.json());
+
+app.post("/posts", (req, res) => {
+  const id = crypto.randomBytes(16).toString("hex");
+  const { content } = req?.body;
+  const post = { id, content };
+  posts[id] = post;
+  res.status(200).send(posts);
+});
+
+app.get("/posts", (req, res) => {
+  res.status(200).send(posts);
 });
 
 app.listen(port, () => {
