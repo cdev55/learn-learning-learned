e86e71a7284c77b51f7c3b7c28b7ca07b0b644bf
Devaansh Chauhan <devaansh.chauhan@mogiio.com>
2024-12-31 23:55:06 +0530

    Learned variables and constant in Go lang res2

---
+++ Go-lay/Resource 2/3-variables/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "boby"
+	// var name = "bobby"
+	name := "bobby"
+
+	var age int = 69
+	// var age = 79
+	// age := 79
+
+	// var isExist bool = true
+	// var isTrue = 5 > 9
+	isTrue := 60 < 80
+
+	fmt.Println(age, name, isTrue)
+}
---
+++ Go-lay/Resource 2/4-constants/constants.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const istrue = false
+
+func main() {
+	const name = "Atul"
+	// name = "bob"
+
+	const age = 25
+
+	fmt.Println(age, istrue)
+}
