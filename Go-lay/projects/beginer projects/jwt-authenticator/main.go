c51eeecb97d893da84154bf8bfb11db4cbb24cec
Devaansh Chauhan <gitdev55@gmail.com>
2025-09-22 23:40:41 +0530

    Added model in jwt-authenticator

---
+++ Go-lay/projects/beginer projects/jwt-authenticator/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"jwt-authenticator/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(context *gin.Context) {
+		context.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(context *gin.Context) {
+		context.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+
+}
