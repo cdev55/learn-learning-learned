dcd0c583a1bf7d3472a2c269e526dad4150fb06f
Devaansh Chauhan <gitdev55@gmail.com>
2025-09-25 11:03:23 +0530

    Added Selection sort

---
+++ DSA/selection-sort.js
@@ -0,0 +1,13 @@
+function selectionSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [arr[j], arr[minIndex]] = [arr[minIndex], arr[j]];
+  }
+  return arr;
+}
