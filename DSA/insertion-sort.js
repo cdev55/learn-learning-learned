e95341d4cc2fba37a23cea8b3def97b63a1df3f7
Devaansh Chauhan <gitdev55@gmail.com>
2025-09-26 22:27:02 +0530

    Added merge sort

--- DSA/insertion-sort.js
+++ DSA/insertion-sort.js
@@ -3,7 +3,7 @@ function insertionSort(arr){
     for(let i=1;i<n;i++){
         let key=arr[i];
         let j=i-1;
-        while(arr[j]>key){
+        while(arr[j]>key && j>=0){
             arr[j+1]=arr[j];
             j--;
         }
---
+++ DSA/merge-sort.js
@@ -0,0 +1,22 @@
+function merge(left,right){
+    let sortedArr=[];
+    while(left.length && right.length){
+        if(left[0]<right[0]){
+            sortedArr.push(left.shift());
+        }else{
+            sortedArr.push(right.shift());
+        }
+    }
+    return [...sortedArr,...left,...right];
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
