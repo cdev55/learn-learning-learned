4722c0bda1e5f167bbc591bbf78a6911be567394
Devaansh Chauhan <gitdev55@gmail.com>
2025-10-15 23:56:00 +0530

    Added binary serch in sorted array

---
+++ DSA/Binary-search/BS-1D/bs-sorted-array.js
@@ -0,0 +1,17 @@
+function binarySearch(arr, target) {
+  const n = arr.length;
+  let low = 0,
+    high = n - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    }
+    if (target > arr[mid]) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
