70aa2f235ba6a2240deaf5482872d8d53cb50b27
Devaansh Chauhan <gitdev55@gmail.com>
2025-10-16 23:24:22 +0530

    Added lower bound

---
+++ DSA/Binary-search/BS-1D/lower-bound.js
@@ -0,0 +1,16 @@
+function lowerBound(arr, x) {
+  const n = arr.length;
+  let low = 0,
+    high = n - 1;
+  let ans = n;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] <= x) {
+      ans = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
