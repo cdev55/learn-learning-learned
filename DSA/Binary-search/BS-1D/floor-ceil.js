c1191ddbd82b698e17bba10019d47617a6a93700
Devaansh Chauhan <gitdev55@gmail.com>
2025-10-17 22:17:46 +0530

    Added floor-ciel

---
+++ DSA/Binary-search/BS-1D/floor-ceil.js
@@ -0,0 +1,40 @@
+function floorCiel(arr, x) {
+  const f = getFloor(arr, x);
+  const c = getCeil(arr, x);
+  return f, c;
+}
+
+function getFloor(arr, x) {
+  const n = arr.length;
+  let low = 0,
+    high = n - 1;
+  let ans = -1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] <= x) {
+      ans = mid;
+      high = mid + 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
+
+
+function getCeil(arr, x) {
+    const n = arr.length;
+    let low = 0,
+      high = n - 1;
+    let ans = -1;
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
+      if (arr[mid] >= x) {
+        ans = mid;
+        high = mid + 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return ans;
+  }
