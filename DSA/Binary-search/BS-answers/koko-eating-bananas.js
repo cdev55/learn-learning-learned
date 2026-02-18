e3c3880889970a986f8381a691ff7dd27e46742f
Devaansh Chauhan <gitdev55@gmail.com>
2026-02-18 21:31:37 +0530

    koko eating bananas

--- DSA/Binary-search/BS-answers/koko-eating-bananas.js
+++ DSA/Binary-search/BS-answers/koko-eating-bananas.js
@@ -1,4 +1,22 @@
-function kokoEatingBananas(arr){
-    const n=arr.length;
+var minEatingSpeed = function(piles, h) {
+    const n=piles.length;
+    let low=0;
+    let high=Math.max(...piles)
+    while(low<=high){
+        const mid=Math.floor((low+high)/2);
+        if(hoursTaken(piles,mid)>h){
+            low=mid+1;
+        }else{
+            high=mid-1
+        }
+    }
+    return low
+};
 
+function hoursTaken(piles,k){
+    let hours=0;
+    for(item of piles){
+        hours=hours+Math.ceil(item/k);
+    }
+    return hours;
 }
