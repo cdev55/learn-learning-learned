8040ad3f5830aa415f3dd33423b6e356144c0853
Devaansh Chauhan <devaansh.chauhan@mogiio.com>
2026-07-11 13:26:58 +0530

    bst: insert and search

---
+++ DSA/tree/bst.js
@@ -0,0 +1,56 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class binarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root === null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      return (this.root = newNode);
+    }
+    return this.insertNode(this.root, newNode);
+  }
+
+  insertNode(root, newNode) {
+    if (root.value > newNode.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else if (root.value < newNode.value) {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+
+  search(root, value) {
+    if (root.value === null) {
+      return false;
+    } else {
+      if (root.value > value) {
+        this.search(root.left, value);
+      } else if (root.value < value) {
+        this.search(root.right, value);
+      } else {
+        return true;
+      }
+    }
+  }
+
+}
